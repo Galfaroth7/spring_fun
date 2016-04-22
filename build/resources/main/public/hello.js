0e0afa4fca2d5f309786ce33fd7f58eb504e4ff3
Galfaroth7 <misiekostap@wp.pl>
2016-04-22 11:40:39 +0200

    No pain no gain

--- build/resources/main/public/hello.js
+++ build/resources/main/public/hello.js
@@ -1,6 +1,7 @@
 function Cheese($scope, $http) {
     $http.get('http://localhost:8080/displayfirst').
         success(function(data) {
+            console.log(data);
             $scope.cheese = data;
         });
 }
